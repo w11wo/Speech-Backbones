ad33558c9aab32b5f37b555cb03ce6e6ba119902
Wilson Wongso <wilsonwong961@gmail.com>
2022-05-25 17:55:19 +0700

    added ID-only CMU; longer fine-tuning

--- Grad-TTS/finetune_params.py
+++ Grad-TTS/finetune_params.py
@@ -10,10 +10,10 @@ from model.utils import fix_len_compatibility
 
 
 # data parameters
-train_filelist_path = "../../mixed/train.txt"
-valid_filelist_path = "../../mixed/valid.txt"
+train_filelist_path = "../../id/train.txt"
+valid_filelist_path = "../../id/valid.txt"
 # test_filelist_path = 'resources/filelists/ljspeech/test.txt'
-cmudict_path = "resources/cmu_dictionary_id_en"
+cmudict_path = "resources/cmu_dictionary_id"
 add_blank = True
 n_feats = 80
 n_spks = 247  # for Libri-TTS filelist and 1 for LJSpeech
@@ -43,11 +43,11 @@ beta_max = 20.0
 pe_scale = 1000  # 1 for `grad-tts-old.pt` checkpoint
 
 # training parameters
-log_dir = "logs/grad-tts-bookbot-ft-mixed"
+log_dir = "logs/grad-tts-bookbot-ft-weildan"
 test_size = 4
-n_epochs = 1000
-batch_size = 8
-learning_rate = 1e-5
+n_epochs = 50000
+batch_size = 16
+learning_rate = 1e-4
 seed = 37
 save_every = 100
 out_size = fix_len_compatibility(2 * 22050 // 256)
