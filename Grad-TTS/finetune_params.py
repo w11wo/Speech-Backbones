c3d0e06ad6bf61a091f203dd1a9de32c4abf3b66
Wilson Wongso <wilsonwong961@gmail.com>
2022-06-02 18:16:10 +0700

    modified ft params for emov

--- Grad-TTS/finetune_params.py
+++ Grad-TTS/finetune_params.py
@@ -10,10 +10,10 @@ from model.utils import fix_len_compatibility
 
 
 # data parameters
-train_filelist_path = "../../id/train.txt"
-valid_filelist_path = "../../id/valid.txt"
+train_filelist_path = "../../en/train.txt"
+valid_filelist_path = "../../en/valid.txt"
 # test_filelist_path = 'resources/filelists/ljspeech/test.txt'
-cmudict_path = "resources/cmu_dictionary_id"
+cmudict_path = "resources/cmu_dictionary"
 add_blank = True
 n_feats = 80
 n_spks = 247  # for Libri-TTS filelist and 1 for LJSpeech
@@ -43,10 +43,10 @@ beta_max = 20.0
 pe_scale = 1000  # 1 for `grad-tts-old.pt` checkpoint
 
 # training parameters
-log_dir = "logs/grad-tts-bookbot-ft-weildan"
+log_dir = "logs/grad-tts-bookbot-ft-amused"
 test_size = 4
-n_epochs = 24000
-batch_size = 8
+n_epochs = 13000
+batch_size = 16
 learning_rate = 1e-4
 seed = 37
 save_every = 1000
